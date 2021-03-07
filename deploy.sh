836ff1b62f8706dc78068806896651b397671146
mrunhap <liubolovelife@gmail.com>
2021-03-07 23:50:36 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+echo -e "\033[0;32mDeploying updates to GitHub Pages...\033[0m"
 hugo
 
 cd public
@@ -12,6 +12,18 @@ if [ $# -eq 1 ]
 fi
 git commit -m "$msg"
 
-git push origin master
+git push origin main
 
 cd ..
+
+echo -e "\033[0;32mDeploying updates to GitHub bookshelf...\033[0m"
+
+git add -A
+
+msg="update"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+git push origin main
